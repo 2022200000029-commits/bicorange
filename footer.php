deb8c3a35fb90ba0f4d780e3b10d4ff5d1296d4d
Mahrab <2022200000029@seu.edu.bd>
2026-06-01 14:10:59 +0600

    Add customer footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<section class="footer">
+
+   <div class="box-container" align="center">
+
+      
+
+      
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 01874108321</p>
+         <p> <i class="fas fa-envelope"></i> bicorange.shop@gmail.com</p>
+         <p> <i class="fas fa-map-marker-alt"></i> Dhaka-1208, Bangladesh </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=61567093935934" target=blank> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.instagram.com/bicorange.shop?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target=blank> <i class="fab fa-instagram"></i> instagram </a>
+
+      </div>
+
+      
+
+   </div>
+
+
+</section>
